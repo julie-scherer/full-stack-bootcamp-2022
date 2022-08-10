4a1e80e4d0117371766f68f5cbc5ccb0201b2206
JulieAnn Scherer <julieannscherer@MacBook-Pro-2021.local>
2022-08-10 12:55:05 -0500

    updated ERD

--- week5/movie_theater/mod2-movie_theater.sql
+++ week5/movie_theater/mod2-movie_theater.sql
@@ -1,25 +1,42 @@
 CREATE TABLE movie (
-	movie_id SERIAL PRIMARY KEY
-	movie_name VARCHAR(50)
-	release_date DATE NOT NULL
-	show_time TIME NOT NULL
-	theater INTEGER NOT NULL
+	movie_id SERIAL PRIMARY KEY,
+	movie_name VARCHAR(50),
+	release_date DATE NOT NULL,
 	age_rating VARCHAR(5)
-)
+);
+
+CREATE TABLE show_time (
+	show_time_id SERIAL PRIMARY KEY,
+	show_time TIME,
+	show_date DATE
+);
+
+CREATE TABLE movie_time (
+	movie_time_id SERIAL PRIMARY KEY,
+	FOREIGN KEY(movie_id) REFERENCES movie(movie_id),
+	FOREIGN KEY(show_time_id) REFERENCES show_time(show_time_id)
+);
 
 CREATE TABLE customer (
-	customer_id SERIAL PRIMARY KEY
-	first_name VARCHAR(50)
-	last_name VARCHAR(50)
-	age INTEGER
+	customer_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	age_ INTEGER, -- accepts NULL values
 	rewards BOOLEAN
-	FOREIGN KEY movie_id REFERENCES movie(movie_id) INTEGER
-)
+--	FOREIGN KEY ticket_id REFERENCES ticket(ticket_id)
+);
 
 CREATE TABLE ticket (
-	ticket_id SERIAL PRIMARY KEY
-	order_date DATE NOT NULL
-	FOREIGN KEY customer_id REFERENCES customer(customer_id)
-	FOREIGN KEY movie_id REFERENCES movie(movie_id)
+	ticket_id SERIAL PRIMARY KEY,
+	order_date DATE NOT NULL,
 	price NUMERIC(4,2)
-)
+--	FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
+--	FOREIGN KEY(movie_id) REFERENCES movie(movie_id);
+);
+
+ALTER TABLE ticket
+ADD FOREIGN KEY(customer_id) REFERENCES customer(customer_id),
+ADD FOREIGN KEY(movie_id) REFERENCES movie(movie_id);
+
+ALTER TABLE customer
+ADD FOREIGN KEY(ticket_id) REFERENCES ticket(ticket_id);
