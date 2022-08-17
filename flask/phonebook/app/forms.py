42bdee2c8ebe24a411630f5128cdb1a3d3170ada
JulieAnn Scherer <julieannscherer@MacBook-Pro-2021.local>
2022-08-17 10:45:47 -0500

    DataRequired() to InputRequired()

--- flask/phonebook/app/forms.py
+++ flask/phonebook/app/forms.py
@@ -1,17 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, Regexp
+from wtforms.validators import InputRequired, Length, Email, Regexp
 
 class Address(FlaskForm):
-    firstname = StringField('First Name',validators=[DataRequired(),Length(min=2,max=20)])
-    lastname = StringField('Last Name',validators=[DataRequired(),Length(min=2,max=25)])
-    phone = StringField('Phone Number',validators=[DataRequired(),Length(min=10,max=14),Regexp('\(?\d{3}\)? ?\d{3}\-?\d{4}')])
+    firstname = StringField('First Name',validators=[InputRequired(),Length(min=2,max=20)])
+    lastname = StringField('Last Name',validators=[InputRequired(),Length(min=2,max=25)])
+    phone = StringField('Phone Number',validators=[InputRequired(),Length(min=10,max=14),Regexp('\(?\d{3}\)? ?\d{3}\-?\d{4}')])
     email = StringField('Email',validators=[Email()])
-    street1 = StringField('Street Line 1',validators=[DataRequired()])
+    street1 = StringField('Street Line 1',validators=[InputRequired()])
     street2 = StringField('Street Line 2')
-    city = StringField('City',validators=[DataRequired()])
-    state = StringField('State',validators=[DataRequired(),Length(2)])
-    zip = StringField('Zip Code',validators=[DataRequired(),Length(min=5,max=10)])
+    city = StringField('City',validators=[InputRequired()])
+    state = StringField('State',validators=[InputRequired(),Length(2)])
+    zip = StringField('Zip Code',validators=[InputRequired(),Length(min=5,max=10)])
     submit = SubmitField('Submit')
 
 def __repr__(self):
