fb2b94edf9df5b28cf517f7ccf0d3888f72fa90f
JulieAnn Scherer <julieannscherer@MacBook-Pro-2021.local>
2022-08-19 10:30:23 -0500

    renamed dir

--- flask/flask_project_template/app/routes.py
+++ flask/flask_project_template/app/routes.py
@@ -1,6 +1,26 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, redirect, url_for, flash
+from app.forms import TemplateForm
+from app.models import User
+
 
 @app.route('/')
+@app.route('/index')
+@app.route('/home', methods=["GET","POST"])
 def home():
-    return render_template('index.html')
+    return "Hello World!"
+
+@app.route('/form')
+def form():
+    form = TemplateForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        
+        db.session.add(user)
+        db.session.commit() # call db commit method to save user to database        
+        
+        flash("Your account has been created! You are now able to log in.", 'success')
+        return redirect(url_for('home'))
+        
+    return render_template('home.html',form=form)
